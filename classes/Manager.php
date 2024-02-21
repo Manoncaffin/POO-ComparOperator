872df8a95207c1251df323d817c2ccb9dc74ba46
Caffin <caffinmanon@gmail.com>
2024-02-21 16:05:40 +0100

    #conflits

--- classes/Manager.php
+++ classes/Manager.php
@@ -8,6 +8,28 @@ class Manager {
         $this->db = $db;
     }
 
+    public function addUser(User $author)
+    {
+        $request = $this->db->prepare('INSERT INTO user (author, password)
+        VALUES (:author, :password)');
+        $request->execute([
+            'author' => $author->getAuthor(),
+            'password' => $author->getPassword(),
+        ]);
+
+        $id = $this->db->lastInsertId();
+     
+        $_SESSION['user_id'] = $id;
+    }
+
+    public function findUserById(User $user)
+    {
+        $request = $this ->db->prepare('SELECT id, author FROM user WHERE id = :id');
+        $request->execute([
+            'id' => $user->getId(),
+        ]);
+    }
+
     public function getAllDestination()
     {
         // return $this->getAllDestination;
@@ -33,7 +55,6 @@ class Manager {
         return $result->fetch();
     }
 
-
     public function getOperatorByDestination()
     {
         // return $this->getOperatorByDestination;
@@ -57,77 +78,4 @@ class Manager {
         return $result->fetchAll();
         
     }
-
-    public function updateOperatorToPremium()
-    {
-        // return $this ->updateOperatorToPremium;
-    }
-
-    public function createTourOperator()
-    {
-        // return $this->createTourOperator;
-    }
-
-    public function createDestination()
-    {
-        // return $this->createDestination;
-    }
-}
-<?php
-
-class Manager {
-    
-    private $db;
-
-    public function __construct($db) {
-        $this->db = $db;
-    }
-
-    public function getAllDestination()
-    {
-        // return $this->getAllDestination;
-    }
-
-    public function getOperatorByDestination()
-    {
-        // return $this->getOperatorByDestination;
-    }
-
-    public function createReview()
-    {
-        // return $this->createReview;
-    }
-
-    public function getReviewByOperatorId()
-    {
-        // return $this->getReviewByOperatorId;
-    }
-
-    public function getAllOperator()
-    {
-        // return $this->getAllOperator;
-    }
-
-    public function addUser(User $author)
-    {
-        $request = $this->db->prepare('INSERT INTO user (author, password)
-        VALUES (:author, :password)');
-        $request->execute([
-            'author' => $author->getAuthor(),
-            'password' => $author->getPassword(),
-        ]);
-
-        $id = $this->db->lastInsertId();
-     
-        $_SESSION['user_id'] = $id;
-    }
-
-    public function findUserById(User $user)
-    {
-        $request = $this ->db->prepare('SELECT id, author FROM user WHERE id = :id');
-        $request->execute([
-            'id' => $user->getId(),
-        ]);
-    }
-    
 }
