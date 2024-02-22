fe32df54c17cd9bf6a0372bb96d38cdfc409b2c4
Caffin <caffinmanon@gmail.com>
2024-02-22 19:05:48 +0100

    form

--- admin.php
+++ admin.php
@@ -6,7 +6,7 @@ require_once('./config/database.php');
 $manager = new Manager($db);
 $operators = $manager->getAllOperator();
 
-var_dump($operators);
+// var_dump($operators);
 
 
 ?>
@@ -22,15 +22,52 @@ var_dump($operators);
     <title>Compar Opérator - Page admin</title>
 </head>
 
-<body>
-    <header>
-        <div class="logo background_logo">
-            <img src="./img/logo_compar_operator.svg" alt="ComparOperator">
-        </div>
-    </header>
+
+<header>
+    <div class="logo background_logo">
+        <img src="./img/logo_compar_operator.svg" alt="ComparOperator">
+    </div>
+</header>
+
+<body class="admin">
 
     <main>
-        <section class="container vh-100 flex-column">
+
+        <!-- <section class="container vh-100 flex-column">
+            <form action="./treatment/treatment_operator.php" method="post" class="form">
+                <div class="form-group admin row d-flex justify-content-start flex-column">
+                    <label for="name" class="col-auto col-form-label col-form-label-sm">Nom du Tour Opérateur</label>
+                    <input type="text" name="name" required>
+                </div>
+
+                <div class="form-group admin row d-flex justify-content-start flex-column">
+                    <label for="link" class="col-auto col-form-label col-form-label-sm">Lien</label>
+                    <input type="text" name="link" required>
+                </div>
+
+                <div class="form-group admin row d-flex justify-content-start flex-column">
+                    <label for="gradeCount" class="col-auto col-form-label col-form-label-sm">Nombre de notes</label>
+                    <input type="number" name="gradeCount" required>
+                </div>
+
+                <div class="form-group admin row d-flex justify-content-start flex-column">
+                    <label for="gradeTotal" class="col-auto col-form-label col-form-label-sm">Total des notes</label>
+                    <input type="number" name="gradeTotal" required>
+                </div>
+
+                <div class="form-group admin row d-flex justify-content-start flex-column">
+                    <label for="isPremium" class="col-auto col-form-label col-form-label-sm">Premium</label>
+                    <select name="isPremium" required>
+                        <option value="1">Oui</option>
+                        <option value="0">Non</option>
+                    </select>
+                </div>
+
+                <div>
+                    <button class="btn mt-3" type="submit">Ajouter Tour Opérateur</button>
+                </div>
+            </form>
+
             <form action="./treatment/treatment_operator.php" method="post" class="form">
                 <div class="form-group admin row d-flex justify-content-start">
                     <label for="text" class="col-auto col-form-label col-form-label-sm">Changer le Tour Opérator</label>
@@ -59,21 +96,11 @@ var_dump($operators);
             <form action="./treatment/treatment_destination.php" method="post" class="form">
                 <div class="form-group admin row d-flex justify-content-start">
                     <label for="text" class="col-auto col-form-label col-form-label-sm">Sélectionner une destination</label>
-                    <select name="location">
-                        <option value="none" selected>--</option>
-                        <option value="Rome">Rome</option>
-                        <option value="Londres">Londres</option>
-                        <option value="Monaco">Monaco</option>
-                        <option value="Tunis">Tunis</option>
-                        <option value="Istanbul">Istanbul</option>
-                        <option value="Beirut">Beirut</option>
-                    </select>
-
                     <select name="tour_operator_id">
                         <option value="none" selected>--</option>
-                        <?php foreach($operators as $operator) {?>
+                        <?php foreach ($operators as $operator) { ?>
                             <option value="<?php echo $operator['id'] ?>"><?php echo $operator['name'] ?> </option>
-                            <?php  }  ?> 
+                        <?php  }  ?>
                     </select>
 
                     <div class="form-group admin row d-flex justify-content-start">
@@ -81,14 +108,113 @@ var_dump($operators);
                         <input type="number" placeholder="€" name="price">
                     </div>
                     <div>
-                    <a href="./treatment/treatment_operator.php" class="text-decoration-none text-black"><button class="btn mt-3" type="submit">Ajouter</button></a>
+                        <a href="./treatment/treatment_operator.php" class="text-decoration-none text-black"><button class="btn mt-3" type="submit">Ajouter</button></a>
                     </div>
                 </div>
             </form>
-        </section>
+
+            </section> -->
+
+        <form action="./treatment/treatment_operator.php" method="post" class="form-admin">
+            <h4>Page admin</h4>
+            <hr>
+            <div class="name-field">
+                <label for="name">Nom du Tour Opérator</label>
+                <input type="text" name="name" required>
+            </div>
+            <div class="name-field">
+                <label for="link">Lien vers le Tour Opérator</label>
+                <input type="text" name="link" required>
+            </div>
+            <div class="name-field">
+                <label for="gradeCount">Nombre de notes</label>
+                <input type="number" name="gradeCount" required>
+            </div>
+            <div class="name-field">
+                <label for="gradeTotal">Total des notes</label>
+                <input type="number" name="gradeTotal" required>
+            </div>
+            <div class="name-field">
+                <label>Premium</label>
+                <select name="isPremium" required>
+                    <option value="none" selected>--</option>
+                    <option value="1">Oui</option>
+                    <option value="0">Non</option>
+                </select>
+            </div>
+
+            <!-- AJOUTER LIEN  -->
+            <div>
+                <a href="#" class="text-decoration-none text-black"><button class="btn mt-3" type="submit">Ajouter Tour Opérator</button></a>
+            </div>
+
+            <hr>
+
+            <div class="name-field">
+                <label for="text">Sélectionner un Tour Opérator</label>
+                <select name="tour_operator_id">
+                    <option value="none" selected>--</option>
+                    <option value="tour_operator_id">Salaun Holidays</option>
+                    <option value="tour_operator_id">Fram</option>
+                    <option value="tour_operator_id">Heliades</option>
+                </select>
+            </div>
+            <div class="name-field">
+                <label for="text">Premium</label>
+                <select name="isPremium" required>
+                    <option value="none" selected>--</option>
+                    <option value="1">Oui</option>
+                    <option value="0">Non</option>
+                </select>
+            </div>
+            <!-- AJOUTER LIEN -->
+            <div>
+                <a href="#" class="text-decoration-none text-black"><button class="btn mt-3" type="submit">Valider les modifications</button></a>
+            </div>
+        </form>
+
+        <hr>
+        <!-- REVOIR -->
+        <form action="./treatment/treatment_destination.php" method="post" class="form-admin">
+            <div class="name-field">
+                <label for="text">Sélectionner une destination</label>
+                <select name="tour_operator_id">
+                    <option value="none" selected>--</option>
+                    <option value="location">Londres</option>
+                    <option value="location">Vienne</option>
+                    <option value="location">Rome</option>
+                    <option value="location">Monaco</option>
+                    <option value="location">Tunis</option>
+                </select>
+
+            </div>
+            
+            <div class="name-field">
+                <label>Ajouter une destination</label>
+                <select name="destination">
+                    <option value="none" selected>--</option>
+                    <?php foreach ($destinations as $destination) { ?>
+                        <option value="<?php echo $destination['location'] ?>"><?php echo $destination['name'] ?> </option>
+                    <?php  }  ?>
+                </select>
+            </div>
+            <!-- FIN _ -->
+
+            <div class="name-field">
+                <label for="text">Prix</label>
+                <input type="number" required placeholder="€" name="price">
+            </div>
+            <div>
+                <a href="./treatment/treatment_operator.php" class="text-decoration-none text-black"><button class="btn mt-3" type="submit">Valider les modifications</button></a>
+            </div>
+        </form>
+
+
+
         <footer>
             <p>Compar Opérator 2023</p>
         </footer>
+
 
 
     </main>
