93f60e80b799efe0fedeca9ba0a80895493b4554
Caffin <caffinmanon@gmail.com>
2024-02-23 14:56:13 +0100

    page index + scss

--- config/database.php
+++ config/database.php
@@ -1,8 +1,8 @@
 <?php
 
 try {
-    // $db = new PDO('mysql:host=localhost;dbname=comparo-simple', 'root', 'root');
-     $db = new PDO('mysql:host=localhost;dbname=comparo-simple', 'root', '');
+    $db = new PDO('mysql:host=localhost;dbname=comparo-simple', 'root', 'root');
+    //  $db = new PDO('mysql:host=localhost;dbname=comparo-simple', 'root', '');
 } catch (PDOException $e) {
     echo 'Erreur de connexion : ' . $e->getMessage();
 }
--- index.php
+++ index.php
@@ -29,48 +29,48 @@ $user = $manager->findUserById($_SESSION['user_id']);
 </head>
 
 <body>
-<div id="index">
-<header id=header>
-        <div class="logo">
-            <a href="./index.php"><img src="./img/logo_compar_operator.svg" alt="Logo ComparOperator"></a>
-        </div>
-        <nav>
-            <ul>
-                <li><a href="./pages/connect_interface.php">Me connecter</a></li>
-                <li><a href="./pages/connect_interface.php">Se déconnecter</a></li>
-            </ul>
-        </nav>
-    </header>
+    <div id="index">
+        <header id=header>
+            <div class="logo">
+                <a href="./index.php"><img src="./img/logo_compar_operator.svg" alt="Logo ComparOperator"></a>
+            </div>
+            <nav>
+                <ul>
+                    <li><p>Bonjour <?php echo $user['author'] ?></p></li>
+                    <li><a href="./pages/connect_interface.php">Me connecter</a></li>
+                    <li><a href="./pages/connect_interface.php">Se déconnecter</a></li>
+                </ul>
+            </nav>
+        </header>
 
-    <div>
-        <h2>Bonjour <?php echo $user['author'] ?>,</h2>
+
+                <h1>Destinations</h1>
+
+            <div class="destinations mt-5">
+
+                <?php foreach ($alldestinations as $alldestination) { ?>
+                    <!-- <?php var_dump($alldestination) ?> -->
+                    <article class="d-flex justify-content-center">
+                        <div class="destination__container">
+                            <div class="destination__image">
+                                <a href="./pages/destination.php?destination_id=<?php echo $alldestination['id']; ?>">
+                                    <img src="./img/<?php echo $alldestination['location'] . ".webp" ?>" alt="image">
+                                </a>
+                            </div>
+
+                            <div class="destination__informations">
+                                <div>
+                                    <p><?php echo $alldestination['location'] ?></p>
+                                    <p> Loreem ipsum, dolor sit amet consectetur adipisicing elit. Maxime quam ex beatae debitis neque.se.
+                                </div>
+                                <div>
+                                    <p>A partir de <?php echo $alldestination['price'] ?> €</p>
+                                </div>
+
+                            </div>
+                    </article>
+                <?php } ?>
+            </div>
     </div>
-    <h1>Destinations</h1>
-    <div class="destinations">
-
-        <?php foreach ($alldestinations as $alldestination) { ?>
-            <!-- <?php var_dump($alldestination) ?> -->
-            <article>
-                <div class="destination__container">
-                    <div class="destination__image">
-                        <a href="./pages/destination.php?destination_id=<?php echo $alldestination['id']; ?>">
-                            <img src="./img/<?php echo $alldestination['location'] . ".webp" ?>" alt="image">
-                        </a>
-                    </div>
-
-                    <div class="destination__informations">
-                        <div>
-                            <p><?php echo $alldestination['location'] ?></p>
-                            <p> Loreem ipsum, dolor sit amet consectetur adipisicing elit. Maxime quam ex beatae debitis neque.se.
-                        </div>
-                        <div>
-                            <p>A partir de <?php echo $alldestination['price'] ?> €</p>
-                        </div>
-
-                    </div>
-            </article>
-        <?php } ?>
-    </div>
-        </div>
     <?php
     include "./partials/footer.php";
